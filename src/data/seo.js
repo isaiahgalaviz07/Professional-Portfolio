581be805d657ac9eda72d00f7c32e13e4eb865db
isaiahgalaviz07 <isaiahgalaviz07@gmail.com>
2024-05-08 15:07:54 -0400

    Initial Publishing of Portfolio
    Initial publishing of my portfolio website

---
+++ src/data/seo.js
@@ -0,0 +1,31 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"For five years I have studied at Saginaw Valley State University, and have recently earned a Bachelor's Degree in Computer Science for my hard work. A lot of the coursework I have done at the University has shaped my knowledge of the technologies I best can utilize with my work, as well as my knowledge on how work is done within my industry.",
+		keywords: ["Isaiah", "Isaiah G", "Isaiah Galaviz"],
+	},
+
+	{
+		page: "about",
+		description:
+			"For five years I have studied at Saginaw Valley State University, and have recently earned a Bachelor's Degree in Computer Science for my hard work. A lot of the coursework I have done at the University has shaped my knowledge of the technologies I best can utilize with my work, as well as my knowledge on how work is done within my industry.",
+			keywords: ["Isaiah", "Isaiah G", "Isaiah Galaviz"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+			keywords: ["Isaiah", "Isaiah G", "Isaiah Galaviz"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			keywords: ["Isaiah", "Isaiah G", "Isaiah Galaviz"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,87 @@
+const INFO = {
+	main: {
+		title: "Digital Portfolio - Isaiah G.",
+		name: "Isaiah Galaviz",
+		email: "isaiahgalaviz07@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		facebook: "https://facebook.com/",
+
+		handshake: "https://handshake.com/",
+		
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/"
+	},
+
+	homepage: {
+		title: "Full-stack web, computer app, and mobile app developer",
+		description:
+			"I specialize in both front-end and backend development. I have knowledge in languages such as C, C#, C++, Java, Javascript, Kotlin, Python, and Swift, as well back-end technologies like Node.js and Django. I have experience in building scalable, secure and reliable applications for a number of host devices and services using the technologies at my disposal. I really enjoy the problem-solving aspects of my work, am always eager to challenge myself with whatever is necessary to get things done, and have a passionate desire to both create high-quality code that follows industry standards, and to constantly learn about ongoing changes within my career field.",
+	},
+
+	about: {
+		title: "My name is Isaiah Galaviz, and I live in Saginaw, Michigan. I seek to contribute to an evergrowing industry and to push my limits with how I can utilize my talents.",
+		description:
+			"For five years I have studied at Saginaw Valley State University, and have recently earned a Bachelor's Degree in Computer Science for my hard work. A lot of the coursework I have done at the University has shaped my knowledge of the technologies I best can utilize with my work, as well as my knowledge on how work is done within my industry.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
